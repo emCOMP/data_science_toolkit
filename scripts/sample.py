6b554eec6fabcd5a855d1ca54327c955eb4da459
jmaddock <skimaddster@gmail.com>
2015-02-10 08:03:33 -0800

    cleaned up sample.py, added classes

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,162 +1,166 @@
 from collections import Counter
 import utils,random,re,nltk,os,csv,config
 
-def _find_tweets(db,rumor):
-    query = config.rumor_terms[rumor]
-    tweet_list = db.find(query)
-    print 'finished query'
-    return tweet_list
+class TweetSampler(object):
 
-def _scrub_tweet(text,scrub_url=True):
-    temp = None
-    s = ur'\u201c' + '@.*?:'
-    while text is not temp:
-        temp = text
-        text = re.sub('RT .*?:','',text).strip()
-        text = re.sub('"@.*?:','',text).strip()
-        text = re.sub(s,'',text).strip()
-        text = re.sub('via @.*?:','',text).strip()
-        text = re.sub('via @.*?\b','',text).strip()
-        text = re.sub('@.*?\b','',text).strip()
-        if scrub_url is True:
-            text = re.sub('http.*?\s|http.*?$','',text).strip()
-    #print text
-    return text
+    def __init__(self,event,_namerumor):
+        # name of the event's db
+        self.event_name = event_name
+        # name of the rumor to sample / compress /expand
+        self.rumor = rumor
+        # db containing all event tweets
+        self.db = utils.mongo_connect(db_name=self.event)
+        # db for raw and finals codes
+        self.code_comparison = utils.mongo_connect(db_name='code_comparison',
+                                                   collection_name=rumor)
+        # db for mapping unique tweets to non-uniques
+        self.compression = utils.mongo_connect(db_name='rumor_compression',
+                                               collection_name=rumor)
+        # db collection for the individual rumor
+        self.rumor_collection = utils.mongo_connect(db_name=self.event,
+                                                    collection_name=rumor)
 
-def _create_sample_old(rumor,num,db,f,scrub_url=True):
+    def _find_tweets(self):
+        query = config.rumor_terms[self.rumor]
+        tweet_list = self.db.find(query)
+        print 'finished query'
+        return tweet_list
 
-    tweet_list = [x for x in _find_tweets(db,rumor)]
-    print 'created list'
+    def _scrub_tweet(text,scrub_url=True):
+        temp = None
+        s = ur'\u201c' + '@.*?:'
+        while text is not temp:
+            temp = text
+            text = re.sub('RT .*?:','',text).strip()
+            text = re.sub('"@.*?:','',text).strip()
+            text = re.sub(s,'',text).strip()
+            text = re.sub('via @.*?:','',text).strip()
+            text = re.sub('via @.*?\b','',text).strip()
+            text = re.sub('@.*?\b','',text).strip()
+            if scrub_url is True:
+                text = re.sub('http.*?\s|http.*?$','',text).strip()
+        #print text
+        return text
 
-    count = 0
-    result = []
-    for tweet in tweet_list:
-        text = _scrub_tweet(text=tweet['text'],scrub_url=True)
-        unique = True
-        for y in result:
-            if nltk.metrics.edit_distance(text,y) < 20:
-                unique = False
-        if unique is True:
-            result.append(text)
-            out = '"%s","%s","%s",\n' % (tweet['id'],
-                                         rumor,
-                                         tweet['text'].replace('"',''))
-            f.write(out.encode('utf-8'))
-            count += 1
-        if count >= num:
-            break
+    def _create_sample_old(self,num,f,scrub_url=True):
 
-    return result
+        tweet_list = [x for x in _find_tweets()]
+        print 'created list'
 
-def _create_sample(rumor,db,dbs,num,f,start=0):
-
-    db = db #utils.mongo_connect(db_name='ebola_cache',collection_name=rumor)
-    dbs = dbs
-    #dbs = [utils.mongo_connect(db_name='ebola'),
-    #       utils.mongo_connect(db_name='ebola2'),
-    #       utils.mongo_connect(db_name='ebola3')]
-    result = []
-    if num == 0:
-        query = {}
-    else:
-        query = {'$and':[{'db_id':{'$gte':start}},{'db_id':{'$lt':start+num}}]}
-    tweet_list = db.find(query)
-
-    for tweet in tweet_list:
-        for x in dbs:
-            full_tweet = x.find_one({'id':tweet['id'][0]})
-            if full_tweet is not None:
-                text = full_tweet['text']
+        count = 0
+        result = []
+        for tweet in tweet_list:
+            text = _scrub_tweet(text=tweet['text'],scrub_url=True)
+            unique = True
+            for y in result:
+                if nltk.metrics.edit_distance(text,y) < 20:
+                    unique = False
+            if unique is True:
                 result.append(text)
-                out = '"%s","%s","%s","%s",\n' % (tweet['db_id'],
-                                                  rumor,
-                                                  tweet['id'][0],
-                                                  text.replace('"',''))
+                out = '"%s","%s","%s",\n' % (tweet['id'],
+                                             rumor,
+                                             tweet['text'].replace('"',''))
                 f.write(out.encode('utf-8'))
+                count += 1
+            if count >= num:
                 break
 
-    return result
+        return result
 
-def create_sample(rumor_list,db=None,dbs=None,num=0,start=0,scrub_url=True,old=False):
-    print 'enter a valid file name:'
-    fname_in = raw_input('>> ')
-    title = "%s.csv" % (fname_in)
-    f = utils.write_to_samples(path=title)
-    f.write('"db_id","rumor","id","text"\n')
+    def _create_sample(self,rumor,db,dbs,num,f,start=0):
 
-    for rumor in rumor_list:
+        result = []
+        if num == 0:
+            query = {}
+        else:
+            query = {'$and':[{'db_id':{'$gte':start}},{'db_id':{'$lt':start+num}}]}
+        tweet_list = self.compression.find(query)
+
+        for tweet in tweet_list:
+            for x in dbs:
+                full_tweet = self.db.find_one({'id':tweet['id'][0]})
+                if full_tweet is not None:
+                    text = full_tweet['text']
+                    result.append(text)
+                    out = '"%s","%s","%s","%s",\n' % (tweet['db_id'],
+                                                      rumor,
+                                                      tweet['id'][0],
+                                                      text.replace('"',''))
+                    f.write(out.encode('utf-8'))
+                    break
+
+        return result
+
+    def create_sample(self,start=0,scrub_url=True,old=False):
+        print 'enter a valid file name:'
+        fname_in = raw_input('>> ')
+        title = "%s.csv" % (fname_in)
+        f = utils.write_to_samples(path=title)
+        f.write('"db_id","rumor","id","text"\n')
+
         if old is True:
-            _create_sample_old(rumor=rumor,num=num,db=db,scrub_url=scrub_url,f=f)
+            _create_sample_old(num=num,scrub_url=scrub_url,f=f)
         else:
-            cache = utils.mongo_connect(db_name=db,collection_name=rumor)
-            _create_sample(rumor=rumor,db=cache,dbs=dbs,num=num,f=f,start=start)
+            _create_sample(num=num,f=f,start=start)
 
-def _compress_tweets(db,cache,rumor):
-    tweet_list = _find_tweets(db,rumor)
-    try:
-        count = cache.find().sort('db_id',-1).limit(1).next()['db_id'] + 1
-    except StopIteration:
-        count = 0
-    for tweet in tweet_list:
-        text = _scrub_tweet(text=tweet['text'],scrub_url=True)
+    def _compress_tweets(self):
+        tweet_list = _find_tweets()
+        try:
+            count = self.compression.find().sort('db_id',-1).limit(1).next()['db_id'] + 1
+        except StopIteration:
+            count = 0
+        for tweet in tweet_list:
+            text = _scrub_tweet(text=tweet['text'],scrub_url=True)
 
-        if cache.find_one({'text':text}) is not None:
-            cache.update({'text':text},
-                         {'$addToSet':{'id':tweet['id']}})
-        else:
-            cache.insert({'db_id':count,
-                          'rumor':rumor,
-                          'text':text,
-                          'id':[tweet['id']]})
-            if count == 0:
-                cache.ensure_index('text')
-            count += 1
+            if self.compression.find_one({'text':text}) is not None:
+                self.compression.update({'text':text},
+                                        {'$addToSet':{'id':tweet['id']}})
+            else:
+                self.compression.insert({'db_id':count,
+                                         'rumor':rumor,
+                                         'text':text,
+                                         'id':[tweet['id']]})
+                if count == 0:
+                    self.compression.ensure_index('text')
+                count += 1
 
-def compress_tweets(db_name,rumor_list,cache_name):
-    for rumor in rumor_list:
-        cache = utils.mongo_connect(db_name=cache_name,collection_name=rumor)
-        db = utils.mongo_connect(db_name=db_name)
+    def compress_tweets(self):
         test = cache.find_one()
         if test:
             print 'uniques db already exists!'
             print 'exiting...'
         else:
-            _compress_tweets(db=db,cache=cache,rumor=rumor)
+            _compress_tweets()
 
-def expand_tweets(db_name,cache_name,code_comparison_name,rumor_list):
-    for rumor in rumor_list:
-        cache = utils.mongo_connect(db_name=cache_name,collection_name=rumor)
-        db = utils.mongo_connect(db_name=db_name,collection_name=rumor)
-        code_comparison = utils.mongo_connect(db_name=code_comparison_name,collection_name=rumor)
-        _expand_tweets(db=db,cache=cache,code_comparison=code_comparison,rumor=rumor)
+    def expand_tweets(self):
+        compressed_list = self.code_comparison.find()
+        for tweet in compressed_list:
+            db_id = int(tweet['db_id'])
+            first_code = tweet.get('first_final','')
+            second_code = tweet.get('second_final',[])
+            tweet_list = self.compression.find_one({'db_id':db_id})
+            for tweets in set(tweet_list['id']):
+                self.rumor_collection.update({'id':tweets},
+                                             {'$push':{'codes':{'rumor':rumor,
+                                                                'first_code':first_code,
+                                                                'second_code':second_code}}})
 
-def _expand_tweets(db,cache,code_comparison,rumor):
-    compressed_list = code_comparison.find()
-    for tweet in compressed_list:
-        db_id = int(tweet['db_id'])
-        first_code = tweet.get('first_final','')
-        second_code = tweet.get('second_final',[])
-        tweet_list = cache.find_one({'db_id':db_id})
-        for tweets in set(tweet_list['id']):
-            db.update({'id':tweets},
-                      {'$push':{'codes':{'rumor':rumor,
-                                         'first_code':first_code,
-                                         'second_code':second_code}}})
-
-def rumor_collection(db_name,rumor_list):
-    for rumor in rumor_list:
-        db = utils.mongo_connect(db_name=db_name)
-        rumor_collection = utils.mongo_connect(db_name=db_name,collection_name=rumor)
+    def rumor_collection(self):
         insert_list = []
-        tweet_list = _find_tweets(db=db,rumor=rumor)
+        tweet_list = _find_tweets()
         for tweet in tweet_list:
             insert_list.append(tweet)
             if len(insert_list) == 1000:
-                rumor_collection.insert(insert_list)
+                self.rumor_collection.insert(insert_list)
                 insert_list = []
-        rumor_collection.insert(insert_list)
+            self.rumor_collection.insert(insert_list)
 
 def main():
+    event = 'sydneysiege'
+    rumor = 'hadley'
+    t = TweetSampler(event_name=event,rumor=rumor)
+
+def old_main():
 
     # list of dbs for creating samples from multiple databases
     #dbs = [utils.mongo_connect(db_name='sydneysiege')]
