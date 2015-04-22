1adeebe3ade446a7ba8a1e5405d11a57726e7c41
Ahmer <ahmer.arif@gmail.com>
2015-04-22 01:03:37 -0700

    - Updated to consider retweet deletions - Lots of readability improvements

--- analysis/patterns.py
+++ analysis/patterns.py
@@ -13,7 +13,7 @@ from collections import Counter
 import csv, random, sys
 import itertools
 
-# Some helper functions
+# Helper functions
 def get_sample_file():
   if len(sys.argv) >= 2:
     return sys.argv[1]
@@ -30,6 +30,21 @@ def write_to_csv(name,data):
       writer = csv.writer(f)
       writer.writerows(data)
 
+def is_affirm(tweet):
+  return tweet[8]=="Affirm"
+
+def is_probably_retweet(tweet):
+  return tweet[10]!="0"
+
+def was_deleted(tweet):
+  return tweet[7]=="DELETED"
+
+def was_probably_deleted_by_user(tweet):
+  return ((not is_probably_retweet(tweet)) or (is_probably_retweet(tweet) and original_tweet_not_deleted(tweet)))
+
+def original_tweet_not_deleted(tweet):
+  return tweet[11]=="FOUND" or tweet[11]==""
+
 # Entry point
 tweets = [tweet for tweet in read_from_csv(get_sample_file())][1:]
 tweeters = list(set([tweet[0] for tweet in tweets]))
@@ -39,14 +54,16 @@ written_tweets=[]
 for tweeter in tweeters: #we iterate over the list of tweeters
   matches = [x for x in tweets if x[0] == tweeter and (x[8]=="Affirm" or x[8]=="Deny")] #we only pick up affirms and denials for the tweeter
   code = ""
-
   for match in matches: # for each matched tweet we check its deletion status and primary code
-    if match[8]=="Affirm":
+    if is_affirm(match):
       code +="A"
     else:
       code +="D"
-    if match[7]!="FOUND":
-      code +="(DEL)"
+    if was_deleted(match):
+      if was_probably_deleted_by_user(match):
+        code +="(DEL)"
+      else:
+        code +="(DEL*)"
 
   group = "" # here we figure out which pattern the user falls under
   if code !="A" and code !="":
@@ -66,7 +83,29 @@ for tweeter in tweeters: #we iterate over the list of tweeters
 
     written_tweeters += [[code] + [group] + matches[0][0:5]] # append results to the output set
     for match in matches:
-      written_tweets += [[match[0]] + match[5:9]]
+      written_tweets += [[match[0]] + 
+                          match[5:7] + 
+                          [match[8]] + 
+                          [was_deleted(match)] + 
+                          [is_probably_retweet(match)] + 
+                          [not original_tweet_not_deleted(match)] + 
+                          [was_deleted(match) and was_probably_deleted_by_user(match)]]
 
-write_to_csv("tweeter", ([["Code"] + ["Group"] + ["Screen Name"] + ["Name"] + ["Followers Count"] + ["Friends Count"] + ["Location"]] + written_tweeters))
-write_to_csv("tweets", ([["Screen Name"] +  ["Created"] + ["Text"] + ["Exists?"] + ["Code"]] + written_tweets))
+write_to_csv("tweeter", ([["Code"] + 
+                          ["Group"] + 
+                          ["Screen Name"] + 
+                          ["Name"] + 
+                          ["Followers Count"] + 
+                          ["Friends Count"] + 
+                          ["Location"]] + 
+                        written_tweeters))
+
+write_to_csv("tweets", ([["Screen Name"] + 
+                          ["Created"] + 
+                          ["Text"] + 
+                          ["Code"] + 
+                          ["Deleted?"] + 
+                          ["Retweet?"] + 
+                          ["Original Tweet Deleted?"] + 
+                          ["Likely Deleted by User?"]] +
+                        written_tweets))
