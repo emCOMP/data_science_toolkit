86a5c3020f5aec0bad94c2de8d5f7d47aae7fa5c
LoganWalls <loganwls@uw.edu>
2015-08-02 13:20:14 -0700

    Added support for column reordering.

--- scripts/TweetExporter.py
+++ scripts/TweetExporter.py
@@ -15,9 +15,13 @@ class TweetExporter(object):
                                 Ex. {'rumor': 'sunil'} will include
                                     an extra column called 'rumor'
                                     with a value of 'sunil'.
+
+        order_override<[str]>: An ordered list of the export columns
+                                which will specif the order in which
+                                the columns are written to the file.
     '''
 
-    def __init__(self, path, export_cols, aux_cols={}):
+    def __init__(self, path, export_cols, aux_cols={}, order_override=None):
         # The columns to include in exported CSVs.
         self.export_cols = export_cols
 
@@ -26,7 +30,20 @@ class TweetExporter(object):
         self.aux_headers = aux_cols.keys()
         self.aux_vals = [aux_cols[k] for k in self.aux_headers]
 
-        # The path to export the tweet CSVs to.
+        # The order in which to output the columns.
+        if order_override is None:
+            # Default Order
+            self.output_order = self.aux_headers + export_cols
+
+        else:
+            # Check to make sure all columns are accounted for.
+            if set(self.aux_headers + export_cols) == set(order_override):
+                self.output_order = order_override
+            else:
+                raise ValueError('order_override does not match \
+                                    export_cols and/or aux_cols')
+
+        # The csvwriter object to write the file.
         self.writer = self.__init_output__(path)
 
         # A cleaner to clean the tweets for output purposes.
@@ -41,7 +58,7 @@ class TweetExporter(object):
     # Initializes the output file and writes the header.
     def __init_output__(self, path):
         # Quote all of our headers.
-        headers = self.export_cols + self.aux_headers
+        headers = self.output_order
         f = open(path, 'wb')
         writer = csv.writer(f)
         writer.writerow(headers)
@@ -52,19 +69,26 @@ class TweetExporter(object):
         Writes a tweet to the TweetExporter's output file.
 
     '''
+
     def export_tweet(self, tweet):
-        # Get the functions to generate the column values.
-        col_generators = [self.__getattribute__(c)
-                          for c in self.export_cols]
 
-        # Generate all of the column values.
-        cols = [generator(tweet) for generator in col_generators]
+        line = []
+        for col in self.output_order:
+            # If the column is a built-in...
+            if col in self.export_cols:
+                # Get the generator function
+                generator = self.__getattribute__(col)
+                # Call it on the provided tweet.
+                line.append(generator(tweet))
 
-        # Add the auxilliary values.
-        cols = cols + self.aux_vals
+            # Otherwise it must be an aux column...
+            elif col in self.aux_headers:
+                # Retrive the provided aux value.
+                header_index = self.aux_headers.index(col)
+                line.append(self.aux_vals[header_index])
 
         # Write to the file.
-        self.writer.writerow(cols)
+        self.writer.writerow(line)
 
 
 ##########################################
@@ -84,11 +108,20 @@ class TweetExporter(object):
         return str(tweet['id'])
 
 
+#####################################
+#     Testing Stuff For Testing     #
+#####################################
+
 def test(db, rumor):
     import utils
     mongo = utils.mongo_connect(db, rumor)
     test = mongo.find({}).limit(10)
-    e = TweetExporter('test.csv', ["db_id", "tweet_id", "text"])
+    e = TweetExporter(
+        'test.csv',
+        ["db_id", "tweet_id", "text"],
+        {'rumor': rumor},
+        ["db_id", "rumor", "tweet_id", "text"]
+    )
     for t in test:
         e.export_tweet(t)
 
@@ -105,4 +138,3 @@ if __name__ == '__main__':
         'rumor_name', help='The name of the rumor to use.', type=str)
     args = parser.parse_args()
     test(args.db_name, args.rumor_name)
-
