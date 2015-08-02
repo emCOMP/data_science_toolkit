cf35efad0dcf021d6f39c3507bb3b4adc011f475
LoganWalls <loganwls@uw.edu>
2015-08-02 11:52:02 -0700

    Added TweetExporter.py to handle writing tweets to files.

---
+++ scripts/TweetExporter.py
@@ -0,0 +1,107 @@
+import csv
+import TweetCleaner
+
+
+class TweetExporter(object):
+
+    '''
+    Parameters:
+        path<str>: The destination file-path to write output.
+        export_cols<[str]>: The columns to include in output.
+
+        aux_cols<{str: str}>: Keys are extra column names, values are
+                                the value to write in that column.
+
+                                Ex. {'rumor': 'sunil'} will include
+                                    an extra column called 'rumor'
+                                    with a value of 'sunil'.
+    '''
+
+    def __init__(self, path, export_cols, aux_cols={}):
+        # The columns to include in exported CSVs.
+        self.export_cols = export_cols
+
+        # Auxilliary Columns
+        # We split them up into two lists because we need to preserve order.
+        self.aux_headers = aux_cols.keys()
+        self.aux_vals = [aux_cols[k] for k in self.aux_headers]
+
+        # The path to export the tweet CSVs to.
+        self.writer = self.__init_output__(path)
+
+        # A cleaner to clean the tweets for output purposes.
+        cleaner_settings = {
+            'scrub_non_ascii': True,
+            'scrub_newlines': True,
+            'scrub_quotes': True
+        }
+        self.cleaner = TweetCleaner(
+            all_ops=False, user_settings=cleaner_settings)
+
+    # Initializes the output file and writes the header.
+    def __init_output__(self, path):
+        # Quote all of our headers.
+        headers = self.export_cols + self.aux_headers
+        writer = csv.writer(path)
+        writer.writerow(headers)
+        return writer
+
+    '''
+    Function:
+        Writes a tweet to the TweetExporter's output file.
+
+    '''
+    def export_tweet(self, tweet):
+        # Get the functions to generate the column values.
+        col_generators = [self.__getattribute__(c)
+                          for c in self.export_cols]
+
+        # Generate all of the column values.
+        cols = [generator(tweet) for generator in col_generators]
+
+        # Add the auxilliary values.
+        cols = cols + self.aux_vals
+
+        # Write to the file.
+        self.writer.writerow(cols)
+
+
+##########################################
+#     Define Column Behaviours Here      #
+##########################################
+
+#   Note: 'tweet' is a single tweet object
+#           in all of the methods below.
+
+    def db_id(self, tweet):
+        return str(tweet['_id'])
+
+    def text(self, tweet):
+        return self.cleaner.clean(tweet['text'])
+
+    def tweet_id(self, tweet):
+        return str(tweet['id'])
+
+
+def test(db, rumor):
+    import utils
+    mongo = utils.mongo_connect(db, rumor)
+    test = mongo.find({}).limit(10)
+    e = TweetExporter('test.csv', ["id", "tweet_id", "text"])
+    for t in test:
+        e.export_tweet(t)
+
+    exit()
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Manages the writing of tweets to ')
+    parser.add_argument(
+        'db_name', help='The name of the database to use.', type=str)
+    parser.add_argument(
+        'rumor_name', help='The name of the rumor to use.', type=str)
+    args = parser.parse_args()
+    test(args['db_name'], args['rumor_name'])
+
