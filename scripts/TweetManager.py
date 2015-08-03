8ad1c3a3ac32a9346a109204e9bb646b3fdbd956
LoganWalls <loganwls@uw.edu>
2015-08-03 12:05:36 -0700

    Changed 'generate_full' to 'generate_coding'; Added tweet-assignment functionality for coding sheets; Added comments.

--- scripts/TweetManager.py
+++ scripts/TweetManager.py
@@ -1,26 +1,21 @@
 import argparse
+import random
+import csv
+import nltk
 import utils
 import config
-import nltk
 from TweetCleaner import TweetCleaner
 from TweetExporter import TweetExporter
 
 
 class TweetManager(object):
 
-    def __init__(
-            self,
-            event,
-            rumor,
-            export_path,
-            export_cols
-    ):
-
+    def __init__(self, args):
         # name of the event's db
-        self.event = event
+        self.event = args.db_name
 
         # name of the rumor to work with
-        self.rumor = rumor
+        self.rumor = args.rumor_name
 
         # db containing all event tweets
         self.db = utils.mongo_connect(db_name=self.event)
@@ -39,16 +34,67 @@ class TweetManager(object):
         }
         self.cleaner = TweetCleaner(
             all_ops=False, user_settings=cleaner_settings)
+
+        # Run action-specific initialization.
+        if args.action == 'generate_training':
+            self.__init_training__(args)
+        elif args.action == 'generate_coding':
+            self.__init_coding__(args)
+
+    def __init_training__(self, args):
         # An exporter to handle tweet export.
         self.exporter = TweetExporter(
-                            export_path,
-                            export_cols,
-                            {'rumor': rumor},
-                            ["db_id", "rumor", "tweet_id", "text"]
-                            )
+            args.export_path,
+            args.export_cols,
+            args.aux_cols,
+            args.col_order
+        )
+
+    def __init_coding__(self, args):
+
+        # Check to see if we have a compression database
+        compression_exists = bool(self.compression.find_one())
+
+        # If we don't have a compression database...
+        if not compression_exists:
+            # Compress before we generate the sheet.
+            self.__compress__()
+
+        self.coders_per_tweet = args.coders
+
+        # Read the Coder Assigments csv.
+        with open(args.assignment_sheet, 'rb') as f:
+            reader = csv.DictReader(f)
+            # Make a dict of {coder_name:load}
+            self.coders = {str(row['coder']): int(row['load'])
+                           for row in reader}
+            # We will keep this list in case we need to assign
+            # extra tweets. (Due to the random assignment
+            # sometimes the load can get shifted by a couple tweets).
+            self.backup_coders = []
+
+        # Check to make sure the assignment numbers add up.
+        tweets_to_code = self.compression.count() * self.coders_per_tweet
+        tweets_assigned = sum(self.coders.values())
+        if tweets_to_code > tweets_assigned:
+            diff = tweets_to_code - tweets_assigned
+            raise ValueError(
+                    str(diff)+' tweets are not assigned. Check your math?'
+                )
+        else:
+            # Make a dict of {coder_name:coder's exporter}
+            self.sheets = {}
+            for coder_name in self.coders.keys():
+                # Create an exporter object for the coder.
+                exporter = TweetExporter(
+                    args.directory + '/' + coder_name + '.csv',
+                    args.export_cols,
+                    args.aux_cols,
+                    args.col_order
+                )
+                self.sheets[coder_name] = exporter
 
     def __create_rumor_collection__(self):
-
         # Create a connection to the database.
         # (This creates a new collection if none exists.)
         rumor_collection = utils.mongo_connect(db_name=self.event,
@@ -129,7 +175,6 @@ class TweetManager(object):
         (The representative is coded, then the codes are propagated
          to the other related tweets.)
     '''
-
     def compress(self, args):
         # Check to make sure compression hasn't already been run
         check = self.compression.find_one()
@@ -150,14 +195,13 @@ class TweetManager(object):
         args.edit_distance <int>: The minimum edit_distance for a tweet
                                     to be considered unique.
     '''
-
     def generate_training(self, args):
         sample_size = args.sample_size
         edit_distance = args.edit_distance
 
         # Get the whole set of tweets.
         tweet_list = self.__find_tweets__()
-        print 'Selecting '+str(sample_size)+' unique tweets...'
+        print 'Selecting ' + str(sample_size) + ' unique tweets...'
 
         # How many tweets we have selected so far.
         count = 0
@@ -201,21 +245,53 @@ class TweetManager(object):
 
     '''
     Function:
-        Exports an entire rumor.
+        Helper for generate_coding:
+            Assigns a tweet to the appropriate number of coders
+            for coding and writes it to their respective csv files.
     '''
+    def __delegate__(self, tweet):
+        # If we need to assign extra tweets (in order to ensure)
+        # each tweet is covered by the desired number of coders.
+        if len(self.coders.keys()) < self.coders_per_tweet:
+            # Bring each backup coder in for 1 more tweet.
+            for b in self.backup_coders:
+                self.coders[b] = 1
 
-    def generate_full(self, args):
+            # Clear the backup list
+            self.backup_coders = []
 
-        # Check to see if we have a compression database
-        compression_exists = bool(self.compression.find_one())
+        # Get the list of availiable coders.
+        avaliable_coders = self.coders.keys()
 
-        # If we don't have a compression database...
-        if not compression_exists:
-            # Compress before we generate the sheet.
-            self.__compress__()
+        for i in range(self.coders_per_tweet):
+            # Choose a coder at random.
+            cur_coder = random.choice(avaliable_coders)
+            # Write the tweet to thier sheet.
+            self.sheets[cur_coder].export_tweet(tweet)
 
+            # Remove them from the avaliable coders
+            # so we don't assign this tweet to the same person.
+            avaliable_coders.remove(cur_coder)
+
+            # Decrement the coder's load by 1.
+            self.coders[cur_coder] -= 1
+
+            # If they've reached the number of tweets
+            # assigned to them, stop assigning them tweets.
+            if self.coders[cur_coder] <= 0:
+                del self.coders[cur_coder]
+
+                # Add them to backup in case we go over.
+                self.backup_coders.append(cur_coder)
+
+    '''
+    Function:
+        Exports an entire rumor to coding sheets.
+    '''
+    def generate_coding(self, args):
         # Get the full list of tweets.
         tweet_list = self.compression.find({})
+
         for tweet in tweet_list:
             # Get the full tweet object from the rumor database.
             # ('tweet' is an object from the compression database so it's
@@ -225,16 +301,17 @@ class TweetManager(object):
             # If the tweet exists...
             if full_tweet is not None:
                 tweet['text'] = full_tweet['text']
-                self.exporter.export_tweet(tweet)
+                self.__delegate__(tweet)
+
 
 # !!!WIP!!!
 def validate_args(action, args):
     # A list of required arguments for each action.
     mappings = {
-                'compress': [],
-                'generate_training': ['sample_size'],
-                'generate_full': []
-                }
+        'compress': [],
+        'generate_training': ['sample_size', 'edit_distance'],
+        'generate_coding': ['coders', 'assignment_sheet']
+    }
     arg_vals = vars(args)
 
     # Verify that a valid action was passed.
@@ -245,23 +322,15 @@ def validate_args(action, args):
                 return False
         return True
     else:
-        raise ValueError('Invalid action specified: '+action)
+        raise ValueError('Invalid action specified: ' + action)
 
 
 def main(args):
-    tm = TweetManager(
-        args.db_name,
-        args.rumor_name,
-        args.export_path,
-        args.export_cols
-    )
-    try:
-        action = tm.__getattribute__(args.action)
-        action(args)
+    tm = TweetManager(args)
+    action = tm.__getattribute__(args.action)
+    action(args)
 
-    except ValueError:
-        print 'Invalid action specified: ', args.action
-        exit()
+    exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -272,22 +341,30 @@ if __name__ == '__main__':
     general = parser.add_argument_group('General')
     general.add_argument(
         'action', help='What to do with the tweets.',
-        choices=['compress', 'generate_training', 'generate_full'],
+        choices=['compress', 'generate_training', 'generate_coding'],
         type=str)
     general.add_argument(
         'db_name', help='The name of the database to use.', type=str)
     general.add_argument(
         'rumor_name', help='The name of the rumor to use.', type=str)
     general.add_argument(
-        '-p', '--export_path', help='File path to use for export.',
-        type=str, required=False, default='./samples/sample.csv')
-    general.add_argument(
-        '-c', '--export_cols', help='Which columns to export.',
+        '-ec', '--export_cols', help='Which columns to export.',
         type=str, required=False,
         nargs='*', default=["db_id", "tweet_id", "text"])
+    general.add_argument(
+        '-aux', '--aux_cols', help='Auxilliary columns (filled with \
+                                    user-specified values.)',
+        type=dict, required=False, default={})
+    general.add_argument(
+        '-co', '--col_order', help='Override the output order of csv columns.',
+        type=str, required=False,
+        nargs='*', default=None)
 
     # Args for generate_training().
     training = parser.add_argument_group('Generate Training')
+    training.add_argument(
+        '-p', '--export_path', help='File path to use for export.',
+        type=str, required=False, default='./samples/sample.csv')
     training.add_argument(
         '-ss', '--sample_size',
         help='Number of tweets desired for the sample.',
@@ -296,6 +373,19 @@ if __name__ == '__main__':
         '-ed', '--edit_distance', help='The minimum edit_distance for a tweet\
                                         to be considered unique.',
         type=int, required=False, default=40)
+
+    coding = parser.add_argument_group('Generate Coding')
+    coding.add_argument(
+        '-dir', '--directory', help='Path to output folder for sheets.',
+        type=str, required=False, default='../sheets')
+    coding.add_argument(
+        '-c', '--coders', help='The number of coders per tweet.',
+        type=int, required=False, default=3)
+    coding.add_argument(
+        '-as', '--assignment_sheet',
+        help='A path to a csv containing coder names and the number\
+                of tweets each will code.',
+        type=str, required=False, default='../sheets/coder_assignments.csv')
 
     args = parser.parse_args()
     main(args)
