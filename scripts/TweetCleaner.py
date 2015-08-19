f977254df9e13634cc4e8b18d73aa743989b9e4a
LoganWalls <loganwls@uw.edu>
2015-08-19 09:45:09 -0700

    Fixing multiple places issue.

---
+++ scripts/TweetCleaner.py
@@ -0,0 +1,205 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem.snowball import EnglishStemmer
+
+
+class TweetCleaner(object):
+    """
+    Processes tweet text using a set of specified operations.
+
+    NOTE:   All ops is applied before user_settings, so
+            it is possible to combine the two.
+
+    Ex:     Passing (all_ops=False, user_settings={'lowercase': True})
+            will apply only the lowercase op.
+
+    Args:
+        all_ops (bool): If this is passed, the default settings will
+                be overridden with the value passed.
+                Ex. If True is passed all ops will be set to True.
+
+        user_settings ({str: bool}): Keys are cleaning operations,
+                                    values are bools:
+                                    Ops which are 'True' will be applied
+                                    to each tweet.
+                                    Ops which are 'False' willl not.
+    """
+
+    def __init__(self, all_ops=None, user_settings={}):
+
+        # The default settings for the Cleaner.
+        self.settings = {
+            'scrub_non_ascii': True,
+            'scrub_url': True,
+            'lowercase': True,
+            'scrub_newlines': True,
+            'scrub_hashtags': False,
+            'scrub_retweet_text': True,
+            'scrub_quotes': True,
+            'scrub_punctuation': False,
+            'scrub_mentions': False,
+            'remove_stopwords': False,
+            'stem_words': False,
+        }
+
+        # The order of this list is the order that
+        # ops will be applied to tweets.
+        self.op_prioirty = [
+            'scrub_non_ascii',
+            'scrub_url',
+            'lowercase',
+            'scrub_newlines',
+            'scrub_hashtags',
+            'scrub_retweet_text',
+            'scrub_quotes',
+            'scrub_punctuation',
+            'scrub_mentions',
+            'remove_stopwords',
+            'stem_words'
+        ]
+
+        # User-specified settings.
+        if all_ops == True:
+            self.settings = {k: True for k in self.settings.keys()}
+        elif all_ops == False:
+            self.settings = {k: False for k in self.settings.keys()}
+
+        self.settings.update(user_settings)
+
+        # Find the ops we will use.
+        enabled_ops = [k for k, v in self.settings.iteritems() if v]
+
+        # Get the enabled ops in order of priority.
+        ordered_ops = [op for op in self.op_prioirty if op in enabled_ops]
+
+        # Map the op names to the actual op functions.
+        self.ops = [self.__getattribute__(op_name)
+                    for op_name in ordered_ops]
+
+        # Run any op specific setup.
+        self.__op_setup__()
+
+    def clean(self, text):
+        """
+        Applies all of the instance's enabled ops to the
+        string passed in as 'text'.
+
+        Args:
+            text (str): The tweet text to be cleaned.
+
+        Returns:
+                (str): The resulting string after all enabled ops
+                        are applied to 'text'.
+        """
+        if text is None:
+            return None
+
+        else:
+            result = text
+            for op in self.ops:
+                result = op(result)
+
+            return result
+
+    def __op_setup__(self):
+        """
+        Provides any op-specific setup required for ops.
+        (Eg. importing new libraries, constructing other objects, etc.)
+
+        Put any op-specific setup operations here.
+            Wrap them in an if statement like so:
+
+                if self.settings[<op_name>]:
+                    <op-specific setup here>
+        """
+        if self.settings['stem_words']:
+            self.stemmer = EnglishStemmer(True)
+
+        if self.settings['remove_stopwords']:
+            self.stops = frozenset(stopwords.words("english"))
+
+##########################################
+#           Define Ops Here              #
+##########################################
+
+    # Removes newline characters.
+    def scrub_newlines(self, text):
+        return text.replace('\n', '')
+
+    # Casts all characters to lowercase.
+    def lowercase(self, text):
+        return text.lower()
+
+    # Scrubs any unicode characters which can't be converted to ASCII.
+    def scrub_non_ascii(self, text):
+        return re.sub(r'[^\x00-\x7F]+', u'', text)
+
+    # Removes URLs
+    def scrub_url(self, text):
+        result = re.sub(
+            r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+            u'', text)
+        return result
+
+    # Removes all types of 'retweet text'.
+    def scrub_retweet_text(self, text):
+        s = ur'\u201c' + '@.*?:'
+        result = text
+        result = re.sub('RT .*?:', '', result).strip()
+        result = re.sub('"@.*?:', '', result).strip()
+        result = re.sub(s, '', result).strip()
+        result = re.sub('via @.*?:', '', result).strip()
+        result = re.sub('via @.*?\b', '', result).strip()
+        result = re.sub('@.*?\b', '', result).strip()
+        return result
+
+    # Removes quotation marks.
+    def scrub_quotes(self, text):
+        return text.replace('"', '')
+
+    # Removes puncutation.
+    def scrub_punctuation(self, text):
+        return re.sub(r'[\.,-\/!$%\^&\*;:{}=\-_`~()]', '', text)
+
+    # Removes user-mentions.
+    # NOTE: This removes the entire mention, not just the @.
+    def scrub_mentions(self, text):
+        return re.sub(r'@\w+', u'', text)
+
+    # Removes hashtags.
+    def scrub_hashtags(self, text):
+        return re.sub(r'#\w+', '', text)
+
+    # Stems all words in the tweet.
+    # NOTE: Applies lowercase and scrub_punctuation
+    #       if they are not already applied.
+    def stem_words(self, text):
+        # Cast to lower case if we have not already.
+        if not self.settings['lowercase']:
+            text = self.lowercase(text)
+
+        # Scrub punctuation if we have not already.
+        if not self.settings['scrub_punctuation']:
+            text = self.scrub_punctuation(text)
+
+        tmp = text.split()
+        stemmed = [self.stemmer.stem(w) for w in tmp]
+        text = ' '.join(stemmed)
+        return text
+
+    # Removes stopwords (are, this, the, is, etc.)
+    # NOTE: Applies lowercase and scrub_punctuation
+    #       if they are not already applied.
+    def remove_stopwords(self, text):
+        # Cast to lower case if we have not already.
+        if not self.settings['lowercase']:
+            text = self.lowercase(text)
+
+        # Scrub punctuation if we have not already.
+        if not self.settings['scrub_punctuation']:
+            text = self.scrub_punctuation(text)
+
+        tmp = text.split()
+        non_stopwords = [w for w in tmp if w not in self.stops]
+        text = ' '.join(non_stopwords)
+        return text
---
+++ scripts/basic.py
@@ -0,0 +1,276 @@
+import datetime
+import utils
+from collections import Counter
+from bson.son import SON
+import csv, random, sys
+
+def write_to_csv(filename, result):
+  with open("{filename!s}_results.csv".format(**locals()), "wb") as f:
+      writer = csv.writer(f)
+      writer.writerows(result)
+
+def total_tweets_over_time(db,fname):
+
+    title = "%s.csv" % (fname)
+    f = utils.write_to_data(path=title)
+    f.write('time,total tweets\n')
+
+    #start = datetime.datetime(2014,10,17)
+    #end = datetime.datetime(2014,11,13)
+    start = db.find().limit(1).sort('created_ts',1).next()['created_ts'].replace(second=0)
+        #end = datetime.datetime(2014,10,4)
+    end = db.find().limit(1).sort('created_ts',-1).next()['created_ts'].replace(second=0) + datetime.timedelta(minutes=1)
+    diff = (end - start).days * 1440
+    for date_start in (start + datetime.timedelta(minutes=n) for n in range(diff)):
+        date_end = date_start + datetime.timedelta(seconds=59)
+        #print "time: %s,%s" % (date_start,date_end)
+
+        raw_data = db.find({
+            "created_ts":{
+                "$gte":date_start,
+                "$lte":date_end
+            },
+        }).count()
+        print raw_data
+        result = '"%s",%d\n' % (date_start,raw_data)
+        f.write(result)
+
+def feature_over_time(db,fname,feature,name,gran):
+
+    title = "%s.csv" % (fname)
+    f = utils.write_to_data(path=title)
+    f.write('time,total tweets\n')
+    if gran == 'hour':
+        #start = datetime.datetime(2014,9,30)
+        start = db.find().limit(1).sort('created_ts',1).next()['created_ts'].replace(minute=0,second=0)
+        #end = datetime.datetime(2014,10,4)
+        end = db.find().limit(1).sort('created_ts',-1).next()['created_ts'].replace(minute=0,second=0) + datetime.timedelta(hours=1)
+        diff = (end - start).days * 24 #1440 for hour
+        for date_start in (start + datetime.timedelta(hours=n) for n in range(diff)):
+            date_end = date_start + datetime.timedelta(minutes=59)
+            #print "time: %s,%s" % (date_start,date_end)
+
+            if feature == 'hashtag':
+                raw_data = db.find({
+                    "created_ts":{
+                        "$gte":date_start,
+                        "$lte":date_end
+                    },
+                    'hashtags':name
+                }).count()
+
+            result = '"%s",%d\n' % (date_start,raw_data)
+            f.write(result)
+
+    elif gran == 'minute':
+        start = db.find().limit(1).sort('created_ts',1).next()['created_ts'].replace(second=0)
+        #end = datetime.datetime(2014,10,4)
+        end = db.find().limit(1).sort('created_ts',-1).next()['created_ts'].replace(second=0) + datetime.timedelta(minutes=1)
+        diff = (end - start).days * 1440
+        for date_start in (start + datetime.timedelta(minutes=n) for n in range(diff)):
+            date_end = date_start + datetime.timedelta(seconds=59)
+            #print "time: %s,%s" % (date_start,date_end)
+
+            if feature == 'hashtag':
+                raw_data = db.find({
+                    "created_ts":{
+                        "$gte":date_start,
+                        "$lte":date_end
+                    },
+                    'hashtags':name
+                }).count()
+
+            result = '"%s",%d\n' % (date_start,raw_data)
+            f.write(result)
+
+def multi_feature_over_time(db,feature,names):
+    for name in names:
+        fname = 'hashtag_%s_over_time' % name
+        feature_over_time(db=db,fname=fname,feature='hashtag',name=name,gran='hour')
+
+def codes_over_time(db_name,codes,rumor,gran='hour'):
+    db = utils.mongo_connect(db_name=db_name,collection_name=rumor)
+    for code in codes:
+        fname = '%s_%s_over_time' % (rumor,code)
+        _codes_over_time(db=db,fname=fname,code=code,gran=gran)
+
+def _codes_over_time(db,fname,code,gran):
+    title = "%s.csv" % (fname)
+    f = utils.write_to_data(path=title)
+    f.write('time,total tweets,total_followers\n')
+    if gran == 'hour':
+        start = db.find().limit(1).sort('created_ts',1).next()['created_ts'].replace(minute=0,second=0)
+        end = db.find().limit(1).sort('created_ts',-1).next()['created_ts'].replace(minute=0,second=0) + datetime.timedelta(hours=1)
+        diff = (end - start).days * 24 #1440 for hour
+        for date_start in (start + datetime.timedelta(hours=n) for n in range(diff)):
+            date_end = date_start + datetime.timedelta(minutes=59)
+            
+	    data = db.find({
+                "created_ts":{
+                    "$gte":date_start,
+                    "$lte":date_end
+                },
+                'codes.first_code':code
+            })
+            
+            result = []
+            for tweet in data:
+              result += [tweet['user']['followers_count']]
+            #total_count = data.count()
+            #result = '"%s",%d,%d\n' % (date_start,total_count,total_followers_count)
+            print result
+            f.write(result)
+
+    elif gran == 'minute':
+        start = db.find().limit(1).sort('created_ts',1).next()['created_ts'].replace(second=0)
+        end = db.find().limit(1).sort('created_ts',-1).next()['created_ts'].replace(second=0) + datetime.timedelta(minutes=1)
+        diff = (end - start).days * 1440
+        result = []
+        for date_start in (start + datetime.timedelta(minutes=n) for n in range(diff)):
+            date_end = date_start + datetime.timedelta(seconds=59)
+       	    data = db.find({
+                "created_ts":{
+                    "$gte":date_start,
+                    "$lte":date_end
+                },
+                'codes.first_code':code
+            })
+            
+            for tweet in data:
+              print tweet
+              result += [[tweet['created_ts'], tweet['retweeted_status']['retweet_count'],tweet['retweeted_status']['retweeted'], tweet['user']['followers_count'], code]]
+            #print result
+            #total_count = data.count()
+        print "result"
+        print len(result)
+        print result[0]
+        print result[1]
+        write_to_csv(code,result)
+
+def top_hashtags(db,top,fname=None,write=False):
+    if write:
+        title = "%s.csv" % (fname)
+        f = utils.write_to_data(path=title)
+        f.write('tag,count\n')
+
+    data = db.find({
+        'counts.hashtags':{
+            '$gt':0
+        }
+    })
+    print 'finished query'
+    count = Counter()
+
+    for x in data:
+        count.update(x['hashtags'])
+    print 'finished counting'
+    if write:
+        for x in count.most_common(top):
+            result = '"%s",%s\n' % (x[0],x[1])
+            f.write(result.encode('utf-8'))
+
+    return count.most_common(top)
+
+def top_urls(db,top,fname=None,write=False):
+    if write:
+        title = "%s.csv" % (fname)
+        f = utils.write_to_data(path=title)
+        f.write('expanded_url,count\n')
+
+    data = db.find({
+        'counts.urls':{
+            '$gt':0
+        }
+    })
+    print 'finished query'
+    count = Counter()
+
+    for x in data:
+        for y in x['entities']['urls']:
+            count.update([y['expanded_url']])
+
+    print 'finished counting'
+    if write:
+        for x in count.most_common(top):
+            result = '"%s",%s\n' % (x[0],x[1])
+            f.write(result.encode('utf-8'))
+
+    return count.most_common(top)
+
+def top_mentions(db,top,fname=None,write=False):
+    if write:
+        title = "%s.csv" % (fname)
+        f = utils.write_to_data(path=title)
+        f.write('name,count\n')
+
+    data = db.find({
+        'counts.user_mentions':{
+            '$gt':0
+        }
+    })
+    print 'finished query'
+    count = Counter()
+
+    for x in data:
+        for y in x['entities']['user_mentions']:
+            count.update([y['name']])
+
+    print 'finished counting'
+    if write:
+        for x in count.most_common(top):
+            result = '"%s",%s\n' % (x[0],x[1])
+            f.write(result.encode('utf-8'))
+
+    return count.most_common(top)
+
+def cooccuring_hashtags(db,num_top_nodes,num_co_nodes,fname=None,write=False):
+    if write:
+        title = "%s.csv" % (fname)
+        f = utils.write_to_data(path=title)
+        f.write('node1,node2,edge\n')
+
+    nodes = top_hashtags(db=db,top=num_top_nodes)
+    network = {}
+    for x in nodes:
+        network[x[0]] = []
+        data = db.find({'hashtags':x[0]})
+        print 'finished query'
+        count = Counter()
+
+        for y in data:
+            count.update(y['hashtags'])
+            print 'finished counting'
+        for y in count.most_common(num_co_nodes):
+            if y[0] not in network:
+                network[x[0]].append(y[0])
+                network[x[0]].append(y[1])
+                if write:
+                    result = '"%s","%s",%s\n' % (x[0],y[0],y[1])
+                    f.write(result.encode('utf-8'))
+    return network
+
+def main():
+    db = utils.mongo_connect(db_name='sydneysiege')
+    db_name = 'sydneysiege'
+    codes = ['Affirm','Deny']
+    #total_tweets_over_time(db=db,fname='total_tweets_over_time_sydneysiege')
+    #top_hashtags(db=db,top=1000,fname='top_hashtags_test',write=True)
+    #top_mentions(db=db,top=1000,fname='sydneysiege_top_mentions',write=True)
+    #cooccuring_hashtags(db=db,num_top_nodes=100,num_co_nodes=100,fname='cooccuring_sydneysiege_1000',write=True)
+    #feature_over_time(db=db,fname='tcot2_over_time',feature='hashtag',name='tcot',gran='hour')
+    codes_over_time(db_name=db_name,codes=codes,rumor='lakemba',gran='minute')
+    print "done"
+    '''multi_feature_over_time(db=db,feature='hashtag',names=['illridewithyou',
+                                                           'martinplace',
+                                                           'prayforsydney',
+                                                           '9news',
+                                                           'breaking',
+                                                           'sydneyhostagecrisis',
+                                                           'auspol',
+                                                           'isis',
+                                                           'news',
+                                                           'tcot'])
+    '''
+
+if __name__ == "__main__":
+    main()
